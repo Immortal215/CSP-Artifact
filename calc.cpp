1fbbc1d194080d5356481db8764a305f6852625e
Sharul Shah <sharulshah@icloud.com>
2024-10-15 09:45:05 -0500

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -1,8 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int calculate(int num1, string operat, int num2) {
+    
+}
+
+
 int main()
 {
-  
-  return 0;
+    vector<int> nums; 
+    vector<char> operats;
+    
+    string line;
+    cin >> line; 
+    
+    for (char value : line) {
+        
+        if (isdigit(value) == true) {
+            
+            nums.push_back(value);
+            
+        } else {
+            
+            operats.push_back(value);
+        }
+        
+    }
+    
+    cout << nums[0] << operats[0];
 }
